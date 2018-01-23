aa3252d9f2d4b415d7117e8afaa43facd9067db2
datavetaren <datavetaren@datavetaren.se>
2018-01-23 16:37:56 +0100

    Test update. Still doesn't work. Still debugging.

--- src/interp/test/pl_files/ex_05_controlflow.pl
+++ src/interp/test/pl_files/ex_05_controlflow.pl
@@ -145,7 +145,7 @@ myifthenelse5(A,B,C) :- myit(A), (myit2(Y), Y = 3 -> B = 42 ; B = 4711), C = 17.
 myifthenelse6(A,B,C) :- (member(A, [1,2,3,4,5]) -> B = 1 ; B = 42), C = 4711.
 member(X,Xs) :- member0(Xs,X).
 member0([X|_],X).
-member0([_|Xs],X) :- member(Xs,X).
+member0([_|Xs],X) :- member0(Xs,X).
 
 ?- myifthenelse6(10, Q24, Q25).
 % Expect: Q24 = 42, Q25 = 4711
