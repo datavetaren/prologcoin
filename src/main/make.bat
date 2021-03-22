557fe57684f167eeca323630c3fea03f55cc7491
datavetaren <datavetaren@datavetaren.se>
2021-03-22 23:12:11 +0100

    Fix module dependencies

--- src/main/make.bat
+++ src/main/make.bat
@@ -3,6 +3,6 @@ SET ROOT=..\..
 SET SUBDIR=main
 SET DOLIB=
 SET DOEXE=prologcoind
-SET DEPENDS=secp256k1 wallet node ec coin global terminal interp db common
+SET DEPENDS=secp256k1 wallet node ec coin global terminal interp db pow common
 SET SCRIPTARGS=%ROOT%\bin\prologcoind.exe
 CALL ..\..\env\make.bat %*
--- src/wallet/make.bat
+++ src/wallet/make.bat
@@ -3,6 +3,6 @@ SET ROOT=..\..
 SET SUBDIR=wallet
 SET DOLIB=wallet
 SET DOEXE=
-SET DEPENDS=secp256k1 common db interp terminal ec coin global node
+SET DEPENDS=secp256k1 common db interp terminal ec coin global node pow
 CALL ..\..\env\make.bat %*
 
